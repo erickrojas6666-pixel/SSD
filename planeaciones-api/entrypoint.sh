065c36f01d834abfd93d56a22c6ac81544c718f4
Erick Rojas <erickrojas6666@gmail.com>
2026-08-09 03:44:01 +0000

    Correcciones seeders

--- planeaciones-api/entrypoint.sh
+++ planeaciones-api/entrypoint.sh
@@ -5,7 +5,7 @@ php artisan config:cache
 php artisan route:cache
 
 # Ejecutar las migraciones automáticamente en la base de datos remota
-php artisan migrate --force
+php artisan migrate --force --seed
 
 # Iniciar el servidor Apache en primer plano
 exec apache2-foreground
