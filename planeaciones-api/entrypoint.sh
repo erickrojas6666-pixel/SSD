300fc9e26958a1b310ecaef449519d66db33ae0b
Erick Rojas <erickrojas6666@gmail.com>
2026-08-09 02:17:57 +0000

    Actualización Dockerfile

---
+++ planeaciones-api/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Limpiar y cachear configuraciones para producción
+php artisan config:cache
+php artisan route:cache
+
+# Ejecutar las migraciones automáticamente en la base de datos remota
+php artisan migrate --force
+
+# Iniciar el servidor Apache en primer plano
+exec apache2-foreground
